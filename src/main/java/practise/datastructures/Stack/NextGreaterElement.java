55f53313305a589ca988ad4ef49c309e2c837300
Srikanth Enuguru <srikanth.enuguru@jda.com>
2018-09-18 11:38:15 +0530

    Next Greater element using Stack

--- src/main/java/practise/datastructures/Stack/NextGreaterElement.java
+++ src/main/java/practise/datastructures/Stack/NextGreaterElement.java
@@ -1,14 +1,18 @@
 package practise.datastructures.Stack;
 
+import java.util.Stack;
+
 public class NextGreaterElement {
 
 	public static void main(String[] args) {
 		int arr[]= {11, 9, 13, 21, 3};
         int n = arr.length;
         printNGEUsingLoops(arr, n);
+        printNGEUsingStack(arr, n);
 	}
 	
 	public static void printNGEUsingLoops(int[] arr, int len) {
+		System.out.println("printNGEUsingLoops");
 		int next;
 		for(int i=0;i<len;i++) {	
 			next=-1;
@@ -19,6 +23,36 @@ public class NextGreaterElement {
 				}
 			}
 			System.out.println(arr[i] + "-->"+next);			
+		}
+	}
+	
+	public static void printNGEUsingStack(int[] arr, int len) {
+		System.out.println("printNGEUsingStack");
+		Stack s= new Stack();
+		s.push(arr[0]);
+		for(int i=1;i<len;i++) {
+			int next = arr[i];
+			if(!s.isEmpty()) {
+				int element = (Integer) s.pop();
+				while(element<next) {
+					System.out.println(element+" --> "+next);
+					if(s.isEmpty()) {
+						break;
+					}
+					element=(Integer) s.pop();
+				}
+				
+				if(element>next) {
+					s.push(element);					
+				}
+			}
+			s.push(next);
+		}
+		
+		while(!s.isEmpty()) {
+			int next=-1;
+			int element = (Integer) s.pop();
+			System.out.println(element+" --> "+next);
 		}
 	}
 
