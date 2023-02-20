9c2327f0a57eb89723f49a583db9adf19b2af9b3
Srikanth Enuguru <sri.enuguru@gmail.com>
2023-02-20 11:01:27 +0530

    Update DiameterOfBT.java

--- src/main/java/practise/datastructures/BinaryTree/DiameterOfBT.java
+++ src/main/java/practise/datastructures/BinaryTree/DiameterOfBT.java
@@ -1,74 +1,46 @@
 package practise.datastructures.BinaryTree;
 
+/**
+ * Given the root of a binary tree, return the length of the diameter of the tree.
+ * <p>
+ * The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+ * <p>
+ * The length of a path between two nodes is represented by the number of edges between them.
+ * <p>
+ * https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/707/traversals-trees-graphs/4669/
+ * https://leetcode.com/problems/diameter-of-binary-tree/solutions/1102557/official-solution/
+ */
 public class DiameterOfBT {
+    private int diameter;
 
-    public static void main(String[] args) {
-        /* creating a binary tree and entering the nodes */
-        BinaryTree tree = new BinaryTree();
-        tree.root = new BinaryTreeNode(1);
-        tree.root.left = new BinaryTreeNode(2);
-        tree.root.right = new BinaryTreeNode(3);
-        tree.root.left.left = new BinaryTreeNode(4);
-        tree.root.left.right = new BinaryTreeNode(5);
-        System.out.println("The diameter of given binary tree is : "
-                + diameter(tree.root));
-        System.out.println("The diameter of given binary tree is : "
-                + diameter_new(tree.root));
+    /**
+     * Let N be the number of nodes in the tree.
+     * <p>
+     * Time complexity: O(N). This is because in our recursion function longestPath, we only enter and exit from each node once.
+     * We know this because each node is entered from its parent, and in a tree, nodes only have one parent.
+     * <p>
+     * Space complexity: O(N). The space complexity depends on the size of our implicit call stack during our DFS,
+     * which relates to the height of the tree. In the worst case, the tree is skewed so the height of the tree is O(N).
+     * If the tree is balanced, it'd be O(logN).
+     */
+    public int diameterOfBinaryTree(TreeNode root) {
+        diameter = 0;
+        longestPath(root);
+        return diameter;
     }
 
+    private int longestPath(TreeNode node) {
+        if (node == null) return 0;
+        // recursively find the longest path in
+        // both left child and right child
+        int leftPath = longestPath(node.left);
+        int rightPath = longestPath(node.right);
 
-    // Function to calculate diameter of tree.
-    // Time Complexity: O(n^2)
-    static int diameter(BinaryTreeNode root) {
+        // update the diameter if left_path plus right_path is larger
+        diameter = Math.max(diameter, leftPath + rightPath);
 
-        /* base case if tree is empty */
-        if (root == null) {
-            return 0;
-        }
-
-        /* get the height of left and right sub trees */
-        int lh = HeightOfBT.height(root.left);
-        int rh = HeightOfBT.height(root.right);
-
-        /* get the diameter of left and right subtrees */
-        int ld = diameter(root.left);
-        int rd = diameter(root.right);
-
-        /* Return max of following three
-          1) Diameter of left subtree
-         2) Diameter of right subtree
-         3) Height of left subtree + height of right subtree + 1 */
-        return Math.max(1 + lh + rh, Math.max(ld, rd));
-    }
-
-    // Function to calculate diameter of tree.
-    // Time Complexity: O(n)
-    static int diameter_new(BinaryTreeNode root) {
-        /* base case if tree is empty */
-        if (root == null) {
-            return 0;
-        }
-
-        int ans = Integer.MIN_VALUE;
-        ans = height(root, ans);
-        return ans;
-    }
-
-    static int height(BinaryTreeNode root, int ans) {
-        /* base case if tree is empty */
-        if (root == null) {
-            return 0;
-        }
-
-        /* get the height of left and right sub trees */
-        int lh = height(root.left, ans);
-        int rh = height(root.right, ans);
-
-        // update the answer, because diameter of a
-        // tree is nothing but maximum value of
-        // (left_height + right_height + 1) for each node
-        ans = Math.max(ans, 1 + lh + rh);
-
-        return 1 + Math.max(lh, rh);
+        // return the longest one between left_path and right_path;
+        // remember to add 1 for the path connecting the node and its parent
+        return Math.max(leftPath, rightPath) + 1;
     }
 }
